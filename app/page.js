cb514d6fad130e25ee6acbaad5b5e65700ad5685
thaito <thai.to@kardiachain.io>
2023-07-26 18:14:12 +0700

    Update pixel link

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <>
-      <img src="https://www.pixelhere.com/et/event.php?advertiser=212168&cid=169036904322220TVNTV428847669224V8a&id=645772&udid=123456&variable=anhcuongg94@gmail.com&value=1&tid=1111111" border="0" width="1" height="1" />
+      <img src="https://www.pixelhere.com/et/event.php?advertiser=212168&cid=169037000222220TVNTV428847668204V85&id=645772&udid=123456&variable=thai.toviet@gmail.com&value=2&tid=2222222" border="0" width="1" height="1" />
       <div className="container">
         <img src="image.jpg" alt="image"/>
         <button className="btn" onClick={handleOpenAd}>Join now</button>

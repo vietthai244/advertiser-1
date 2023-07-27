5b09aa4c949733bdf1e7ceeccaebfbe2a227fc2e
thaito <thai.to@kardiachain.io>
2023-07-27 10:42:21 +0700

    Update pixel link

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 "use client"
 
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import uniqid from "uniqid";
+import { uuid } from "uuidv4";
+import sha from "sha256";
 
 export default function Home() {
+  const uuid = uuid()
+  const cid = uniqid()
+  const tid = sha(uuid + new Date())
   const router = useRouter()
-  const params = useSearchParams()
 
 
   const handleOpenAd = () => {
@@ -13,7 +18,8 @@ export default function Home() {
 
   return (
     <>
-      <img src="https://www.pixelhere.com/et/event.php?advertiser=212168&cid=169037257822220TVNTV428847668204V9b&id=824d26&udid=22003322&value=0.1&tid=22222" border="0" width="1" height="1" />
+      {/*<img src={`https://www.pixelhere.com/et/event.php?advertiser=212168&cid=${cid}&id=645772&udid=${uuid}&variable=anhcuongg94@gmail.com&value=1&tid=${tid}`} border="0" width="1" height="1" />*/}
+      <img src="https://www.pixelhere.com/et/event.php?advertiser=212168&cid=169042929322220TVNTV428847668204V61&id=898745&value=1" border="0" width="1" height="1" />
       <div className="container">
         <img src="image.jpg" alt="image"/>
         <button className="btn" onClick={handleOpenAd}>Join now</button>
--- app/redirect/page.jsx
+++ app/redirect/page.jsx
@@ -2,24 +2,33 @@
 
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import sha from "sha256";
+import uniqid from "uniqid";
+import { uuid } from "uuidv4";
 
 export default function RedirectingPage() {
+  const cid = uniqid()
+  const uuid = uuid()
+  const tid = sha(uuid + new Date())
+  const max = 20
+  const min = 1
+  const price = Math.floor(Math.random() * (max - min)) + min
+
   const params = useSearchParams()
-
-  useEffect(() => {
-
-  }, [])
 
   useEffect(() => {
     const url = params.get("url")
     if (!url) return
     setTimeout(() => {
-      window.open(url)
-      window.close()
+      // window.open(url)
+      // window.close()
     }, 2000)
   }, [params])
 
   return (
-    <h1 style={{ textAlign: "center" }}>Redirecting...</h1>
+    <div>
+      <img src={`https://www.pixelhere.com/et/event.php?advertiser=212168&cid=${cid}&id=824d26&udid=${uuid}&value=${price}&tid=${tid}`} border="0" width="1" height="1" />
+      <h1 style={{ textAlign: "center" }}>Redirecting...</h1>
+    </div>
   )
 };

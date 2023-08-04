5ff4456d9305e9731277ee04f36afd609d1b1fdb
thaito <thai.to@kardiachain.io>
2023-08-04 11:26:40 +0700

    Test tracking code

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <img src={`https://www.pixelhere.com/et/event.php?advertiser=212168&cid=${cid}&id=898745&value=1`} border="0" width="1" height="1" />
-      <img src="https://backend-dev.lunargumble.lol/events/pixel?cid=&id=5&advertiser_id=9&variable=&value=&tid=&udid=" border="0" width="1" height="1" />
+      <img src="https://backend-dev.lunargumble.lol/events/pixel?cid=click&id=6&advertiser_id=9&variable=personal&value=10&tid=11&udid=22" border="0" width="1" height="1" />
       <div className="container">
         <img src="image.jpg" alt="image"/>
         <button className="btn" onClick={handleOpenAd}>Join now</button>

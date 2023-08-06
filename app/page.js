44143da51798a4a78d1d95878e4a6ba0ad6441bc
thaito <thai.to@kardiachain.io>
2023-08-06 15:51:55 +0700

    Test tracking code

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import { useRouter, useSearchParams } from 'next/navigation'
+import uniqid from "uniqid";
 
 export default function Home() {
   const router = useRouter()
@@ -15,7 +16,8 @@ export default function Home() {
       <img src={`https://www.pixelhere.com/et/event.php?advertiser=212168&cid=${cid}&id=898745&value=1`} border="0" width="1" height="1" />
       {/*<img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=click&id=6&advertiser_id=9&variable=personal&value=10&tid=11&udid=22" border="0" width="1" height="1" />*/}
       {/*<img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=&id=5&advertiser_id=9&variable=personal&value=109&tid=tx10000&udid=udid1000" border="0" width="1" height="1" />*/}
-      <img src={`https://backend-dev.lunargumble.lol/api/events/pixel?cid=${cid}&id=5&advertiser_id=9&variable=personal&value=109&tid=tx10000&udid=udid1000`} border="0" width="1" height="1" />
+      <img src={`https://backend-dev.lunargumble.lol/api/events/pixel?cid=${uniqid()}&id=5&advertiser_id=9&variable=personal&value=109&tid=tx10000&udid=udid1000`} border="0" width="1" height="1" />
+      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=11&id=9&advertiser_id=2&variable=111&value=1111&tid=2&udid=22" border="0" width="1" height="1" />
       <div className="container">
         <img src="image.jpg" alt="image"/>
         <button className="btn" onClick={handleOpenAd}>Join now</button>

2b7dc39498391c53c10f7a1ef52a1999eabf08cd
vietthai244 <thai.toviet@gmail.com>
2023-11-20 16:20:35 +0700

    Update pixel tracking

--- app/page.js
+++ app/page.js
@@ -36,10 +36,7 @@ export default function Home() {
       {/*<img*/}
       {/*  src={`https://backend-dev.lunargumble.lol/api/events/pixel?cid=${cid}&id=9&advertiser_id=2&variable=homepage_neargiveaway&value=${price1}&tid=${tid1}&udid=${udid1}`}*/}
       {/*  border="0" width="1" height="1"/>*/}
-      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=a&id=17&advertiser_id=16&variable=&value=&tid=b&udid=" border="0" width="1" height="1" />
-      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=&id=20&advertiser_id=18&variable=&value=&tid=&udid=" border="0" width="1" height="1" />
-      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=&id=19&advertiser_id=17&variable=&value=&tid=&udid=" border="0" width="1" height="1" />
-      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=&id=18&advertiser_id=16&variable=&value=&tid=&udid=" border="0" width="1" height="1" />
+      <img src="https://backend-dev.lunargumble.lol/api/events/pixel?cid=&id=21&advertiser_id=19&variable=&value=&tid=&udid=" border="0" width="1" height="1" />
       <div className="container">
         <img src="image.jpg" alt="image"/>
         <button className="btn" onClick={handleOpenAd}>Join now</button>
